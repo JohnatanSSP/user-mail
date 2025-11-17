b146eeea29dd1d98a209e7701e59b0af1ddb8efa
Johnatan S S Paixão <johnatan.sousa@gmail.com>
2025-11-17 18:29:22 -0300

    feat: make a enums

---
+++ Emails_InGress/src/main/java/johnatanSSP/Emails_InGress/enums/EmailStatus.java
@@ -0,0 +1,14 @@
+package johnatanSSP.Emails_InGress.enums;
+
+public enum EmailStatus {
+    PENDING,
+    SENT,
+    RECEIVED,
+    FAILED,
+    OPENED,
+    CLOSED,
+    CLICKED,
+    DELIVERED,
+    COMPLETED,
+    CANCELED,
+}
