67b7384a181db83555eb2c5e486a51fd5f915a2c
Johnatan S S Paixão <johnatan.sousa@gmail.com>
2025-11-17 18:29:51 -0300

    feat: EmailConsumer.java

---
+++ Emails_InGress/src/main/java/johnatanSSP/Emails_InGress/consumer/EmailConsumer.java
@@ -0,0 +1,15 @@
+package johnatanSSP.Emails_InGress.consumer;
+
+import johnatanSSP.Emails_InGress.domain.EmailModel;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailConsumer {
+
+    @RabbitListener
+    public void listenEmailQueue(@Payload String menssage) {
+        System.out.println("Listening email queue" + menssage);
+    }
+}
