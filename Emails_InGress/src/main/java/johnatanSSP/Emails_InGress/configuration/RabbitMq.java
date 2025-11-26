096d0476d1c76eb74478ff9cab580b0800380fb9
Johnatan S S Paixão <johnatan.sousa@gmail.com>
2025-11-26 18:54:32 -0300

    feat: create bean for consumer and refactor listener

--- Emails_InGress/src/main/java/johnatanSSP/Emails_InGress/configuration/RabbitMq.java
+++ Emails_InGress/src/main/java/johnatanSSP/Emails_InGress/configuration/RabbitMq.java
@@ -1,6 +1,8 @@
 package johnatanSSP.Emails_InGress.configuration;
 
 //import org.springframework.amqp.rabbit.annotation.Queue;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.amqp.core.Queue;
@@ -12,5 +14,12 @@ public class RabbitMq {
 
     public Queue queue() {
         return new Queue(queueName,true);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        return new Jackson2JsonMessageConverter(objectMapper);
     }
 }
--- Emails_InGress/src/main/java/johnatanSSP/Emails_InGress/consumer/EmailConsumer.java
+++ Emails_InGress/src/main/java/johnatanSSP/Emails_InGress/consumer/EmailConsumer.java
@@ -1,6 +1,7 @@
 package johnatanSSP.Emails_InGress.consumer;
 
 import johnatanSSP.Emails_InGress.domain.EmailModel;
+import johnatanSSP.Emails_InGress.dto.EmailDto;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
@@ -9,7 +10,8 @@ import org.springframework.stereotype.Component;
 public class EmailConsumer {
 
     @RabbitListener(queues = "email_queue")
-    public void listenEmailQueue(@Payload String menssage) {
-        System.out.println("Listening email queue" + menssage);
+    public void listenEmailQueue(@Payload EmailDto emailDto) {
+
+        System.out.println("Listening email queue" + emailDto.emailTo());
     }
 }
