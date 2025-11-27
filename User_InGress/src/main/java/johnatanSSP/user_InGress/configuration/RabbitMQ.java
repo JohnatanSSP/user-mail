3c4270468d01d765645e6d356fa88395a1aa486e
Johnatan S S Paixão <johnatan.sousa@gmail.com>
2025-11-27 18:11:55 -0300

    feat: implements direct exchange

--- User_InGress/src/main/java/johnatanSSP/user_InGress/configuration/RabbitMQ.java
+++ User_InGress/src/main/java/johnatanSSP/user_InGress/configuration/RabbitMQ.java
@@ -1,20 +1,11 @@
-package johnatanSSP.Emails_InGress.configuration;
+package johnatanSSP.user_InGress.configuration;
 
-//import org.springframework.amqp.rabbit.annotation.Queue;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.amqp.core.Queue;
-
-@Configuration
-public class RabbitMq {
-    private final String queueName = "email_queue";
 
 
-    public Queue queue() {
-        return new Queue(queueName,true);
-    }
+public class RabbitMQ {
 
     @Bean
     public Jackson2JsonMessageConverter messageConverter() {
--- User_InGress/src/main/java/johnatanSSP/user_InGress/producer/UserProducer.java
+++ User_InGress/src/main/java/johnatanSSP/user_InGress/producer/UserProducer.java
@@ -1,4 +1,12 @@
 package johnatanSSP.user_InGress.producer;
 
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+
 public class UserProducer {
+
+    final RabbitTemplate rabbitTemplate;
+    public UserProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+    
 }
