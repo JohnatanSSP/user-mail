54550b3f7e54f994f1b4cd59c35896c3bdd37754
Johnatan S S Paixão <johnatan.sousa@gmail.com>
2025-11-27 23:58:10 -0300

    Update producer

--- User_InGress/src/main/java/johnatanSSP/user_InGress/producer/UserProducer.java
+++ User_InGress/src/main/java/johnatanSSP/user_InGress/producer/UserProducer.java
@@ -1,12 +1,19 @@
 package johnatanSSP.user_InGress.producer;
 
+import johnatanSSP.user_InGress.domain.UserModel;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
 
+@Component
 public class UserProducer {
 
     final RabbitTemplate rabbitTemplate;
     public UserProducer(RabbitTemplate rabbitTemplate) {
         this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendUserCreatedEvent (UserModel userModel){
+        rabbitTemplate.convertAndSend("user.created", event);
     }
     
 }
