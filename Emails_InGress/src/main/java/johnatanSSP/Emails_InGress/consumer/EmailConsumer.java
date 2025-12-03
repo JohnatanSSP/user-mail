84731a94b9686c531ac9d827ddab06e0b730ab93
Johnatan S S Paixão <johnatan.sousa@gmail.com>
2025-12-03 18:18:37 -0300

    feat: configuration .env and SMTP google

--- Emails_InGress/src/main/java/johnatanSSP/Emails_InGress/consumer/EmailConsumer.java
+++ Emails_InGress/src/main/java/johnatanSSP/Emails_InGress/consumer/EmailConsumer.java
@@ -3,6 +3,7 @@ package johnatanSSP.Emails_InGress.consumer;
 import johnatanSSP.Emails_InGress.domain.EmailModel;
 import johnatanSSP.Emails_InGress.dto.EmailDto;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.BeanUtils;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
@@ -11,7 +12,7 @@ public class EmailConsumer {
 
     @RabbitListener(queues = "email_queue")
     public void listenEmailQueue(@Payload EmailDto emailDto) {
-
-        System.out.println("Listening email queue" + emailDto.emailTo());
+        var email = new EmailModel();
+        BeanUtils.copyProperties(emailDto, email);
     }
 }
